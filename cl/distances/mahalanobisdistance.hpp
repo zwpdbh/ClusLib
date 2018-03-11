60ca08fa9a452d274cf15455a479c44a0e44aade
zwpdbh <hyperion_z@outlook.com>
2018-03-11 22:22:25 +1300

    >  c l / u t i l i t i e s / m a t r i x . h p p

--- cl/distances/mahalanobisdistance.hpp
+++ cl/distances/mahalanobisdistance.hpp
@@ -1,4 +1,8 @@
 #ifndef cluslib_mahalanobisdistance_hpp
 #define cluslib_mahalanobisdistance_hpp
 
+#include "cl/distances/distance.hpp"
+#include "cl/errors.hpp"
+#include "cl/types.h"
+
 #endif // !cluslib_mahalanobisdistance_hpp
